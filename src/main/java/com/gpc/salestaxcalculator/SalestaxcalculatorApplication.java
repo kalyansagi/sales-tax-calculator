637ebc6b0f86ba0546b1fd77a2c81f7c2c891681
vxs8rzl <venkata_kalyan_krishna_varma_sagi@homedepot.com>
2021-03-02 15:55:08 -0500

    startup and gitignore

---
+++ src/main/java/com/gpc/salestaxcalculator/SalestaxcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.gpc.salestaxcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalestaxcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalestaxcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gpc/salestaxcalculator/SalestaxcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpc.salestaxcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalestaxcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
